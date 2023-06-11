4eca25c3acc969d3e0029bc01a8ccef4f2a1fd6a
GustavoTavaresBraga <gustavobraga1706@gmail.com>
2023-06-11 20:03:18 -0300

    Jogo base

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import pygame
 import sys
 from settings import *
 from map import *
+from player import *
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pygame.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps():.1f}')
 
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pygame.event.get():
