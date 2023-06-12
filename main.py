792a6042c9c5d423dd2483eb2b18e29ec3f99967
GustavoTavaresBraga <gustavobraga1706@gmail.com>
2023-06-12 14:14:47 -0300

    .

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.raycasting = RayCasting(self)
 
     def update(self):
         self.player.update()
+        self.raycasting.update()
         pygame.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps():.1f}')
--- player.py
+++ player.py
@@ -48,9 +48,9 @@ class Player:
             self.y += dy
 
     def draw(self):
-        pygame.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
-                        (self.x * 100 + WIDTH * cos(self.angle),
-                         self.y * 100 + WIDTH * sin(self.angle)), 2)
+        # pygame.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+        #                 (self.x * 100 + WIDTH * cos(self.angle),
+        #                  self.y * 100 + WIDTH * sin(self.angle)), 2)
         pygame.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
 
     def update(self):
@@ -58,7 +58,7 @@ class Player:
 
     @property
     def pos(self):
-        return self.x. self.y
+        return self.x, self.y
     
     @property
     def map_pos(self):
