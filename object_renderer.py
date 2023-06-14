75f878771fc25659a56c633eb06d1d32f13f44b7
GustavoTavaresBraga <gustavobraga1706@gmail.com>
2023-06-14 14:30:37 -0300

    .

--- object_renderer.py
+++ object_renderer.py
@@ -7,6 +7,14 @@ class ObjectRenderer:
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
 
+    def draw(self):
+        self.render_game_objects()
+
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pygame.image.load(path).convert_alpha()
@@ -14,9 +22,9 @@ class ObjectRenderer:
     
     def load_wall_textures(self):
         return {
-            1: self.get_texture('textures/1.png'),
-            2: self.get_texture('textures/2.png'),
-            2: self.get_texture('textures/3.png'),
-            4: self.get_texture('textures/4.png'),
-            5: self.get_texture('textures/5.png')
+            1: self.get_texture('textures/texture1.png'),
+            2: self.get_texture('textures/texture2.png'),
+            2: self.get_texture('textures/texture3.png'),
+            4: self.get_texture('textures/texture4.png'),
+            5: self.get_texture('textures/texture5.png')
         }
